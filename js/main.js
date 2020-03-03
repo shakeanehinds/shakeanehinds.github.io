e61e7d785147ced73d0d322f1d62ed12dec5a65d
shakeanehinds <44011884+shakeanehinds@users.noreply.github.com>
2020-03-03 01:46:16 -0500

    Added toggle for hover effect

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ window.addEventListener('scroll', function (e) {
   });
   
   // ======= Handling menu collor changes on scrolling =====//
+
+//   Remove hover effect from hero section
+    // needs fixing, check button should toggle when clicked
+function kill_motion(){
+    right = document.getElementById('right');
+    left = document.getElementById('left');
+    times = document.getElementById('timesbtn');
+    times.classList.remove('fa-times-circle');
+    times.classList.add('fa-check-circle')
+    right.classList.toggle('right-hov');
+    left.classList.toggle('left-hov');
+}
+
+//   Remove hover effect from hero section
